de75ea23f0c7167657e31b344aafca63e4ac2dee
Olly Larkin <ollylemonlarkin@gmail.com>
2020-01-02 20:51:50 +0000

    1

---
+++ w1/1/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+// ---------- Functions ----------------
+//--------------------------------------
+
+[<EntryPoint>]
+let main argv =
+    // ---------- Tests ----------------
+    //----------------------------------
+    Console.ReadKey() |> ignore // not needed running from Visual Studio
+    0 // return an integer exit code
