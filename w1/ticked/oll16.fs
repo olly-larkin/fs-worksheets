75512c5cefb8e691413d3165eb6169074567dbda
Olly Larkin <ollylemonlarkin@gmail.com>
2020-01-13 11:30:58 +0000

    Ticked1

--- w1/ticked/oll16.fs
+++ w1/ticked/oll16.fs
@@ -1,12 +1,14 @@
-let fac x = List.reduce (*) [1 .. x]
+let fact x = 
+    List.reduce (*) [1 .. x]
 
-let sum (f: int -> float) n =
+let sum f n =
     [0 .. n] |> List.map f |> List.reduce (+)
 
-let g x i =
-    ((-1.0)**(float i)) * (x ** (2.0 * (float i) + 1.0)) / (float (fac (2 * i + 1)))
-
-let sin (x: float, n: int) =
+let sinSeries x n =
+    let g x i =
+        let num x i = x ** (2.0 * (float i) + 1.0)
+        let den i = fact (2 * i + 1)
+        (-1.0 ** (float i)) * (num x i) / (float (den i))
     if n >= 1 
     then sum (g x) n
     else 0.0
