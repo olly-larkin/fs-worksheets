76539cd5f4f48ddd2454e7fa7fd1fa07623e6c6c
Olly Larkin <ollylemonlarkin@gmail.com>
2020-01-16 11:31:14 +0000

    q12

---
+++ w2/q12/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let reverseList list =
+    List.fold (fun lst elem -> elem::lst) [] list 
+ 
+[<EntryPoint>]
+let main argv =
+    reverseList [1;2;3;4] |> printfn "%A"
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
