0dfa12836ed9b6887991b4eb3fd38f39648bbf7c
Olly Larkin <ollylemonlarkin@gmail.com>
2020-01-16 13:22:58 +0000

    q30

---
+++ w2/q30/Program.fs
@@ -0,0 +1,20 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let mode lst =
+    let count =
+        lst
+        |> List.countBy id
+        |> List.sortByDescending snd
+    match count with
+    | [] -> ([], 0)
+    | (_, cnt)::_ ->
+        let modes = List.takeWhile (fun (_, cnt') -> cnt' = cnt) count
+        List.map fst modes , cnt
+
+[<EntryPoint>]
+let main argv =
+    0 |> printfn "%A"
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
