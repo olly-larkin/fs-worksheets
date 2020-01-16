176a020a123582988a102dde41fe41bd12638266
Olly Larkin <ollylemonlarkin@gmail.com>
2020-01-16 13:09:24 +0000

    q29

---
+++ w2/q28/Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let gap lst =
+    lst
+    |> List.sort
+    |> List.pairwise
+    |> List.map (fun (a,b) -> b - a)
+    |> List.max
+
+[<EntryPoint>]
+let main argv =
+    gap [1;3;6;3;2] |> printfn "%A"
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
---
+++ w2/q29/Program.fs
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let gap =
+    List.sort
+    >> List.pairwise
+    >> List.map (fun (a,b) -> b - a)
+    >> List.max
+
+[<EntryPoint>]
+let main argv =
+    gap [1;3;6;3;2] |> printfn "%A"
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
