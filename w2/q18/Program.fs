4146ba116978cca5fd01effca8c1618daa84ae69
Olly Larkin <ollylemonlarkin@gmail.com>
2020-01-16 12:06:30 +0000

    q18

---
+++ w2/q18/Program.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let subsets lst =
+    let rec subsets' l subs =
+        match l with
+        | hd::tl ->
+            List.allPairs subs [[hd];[]]
+            |> List.map ((<||) List.append)
+            |> subsets' tl
+        | [] -> [[]]
+    subsets' lst [[]]
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
