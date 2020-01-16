8260e5e584ed1d834bb776eeb243f62f6a9797c8
Olly Larkin <ollylemonlarkin@gmail.com>
2020-01-16 14:11:09 +0000

    q33

---
+++ w2/q33/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let insertElement i x (lst: 'a list) =
+    lst.[..i-1] @ [x] @ lst.[i..]
+
+[<EntryPoint>]
+let main argv =
+    insertElement 3 10 [0;1;2;3;4;5;6;7] |> printfn "%A"
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
