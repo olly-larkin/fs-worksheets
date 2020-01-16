597f485a6aa33c32a2efd678cd8284d700863d2a
Olly Larkin <ollylemonlarkin@gmail.com>
2020-01-16 11:14:59 +0000

    q5

---
+++ w2/q5/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let negPosOr0 list =
+    List.tryFind ((>) 0) list
+    |> Option.orElseWith (fun () -> List.tryFind ((<) 0) list)
+    |> Option.defaultValue 0
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
