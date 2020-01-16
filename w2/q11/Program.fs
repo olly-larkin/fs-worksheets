78ea9a4d94f3ea4e8328d8b0af0a682cc5e7188e
Olly Larkin <ollylemonlarkin@gmail.com>
2020-01-16 11:25:42 +0000

    q11

---
+++ w2/q11/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let fact n = List.fold (*) 1 [2 .. n]
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
---
+++ w2/q8/Program.fs
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let rec filter f lst =
+    match lst with
+    | hd::tl when f hd -> hd::(filter f tl)
+    | _::tl -> filter f tl
+    | [] -> []
+
+[<EntryPoint>]
+let main argv =
+    filter ((<) 0) [1;-1;10;5;-5] |> printfn "%A" 
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
