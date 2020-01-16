218ce6930a165c32915e53c56f2524a93e258ea7
Olly Larkin <ollylemonlarkin@gmail.com>
2020-01-16 12:11:45 +0000

    q19

--- w2/q19/Program.fs
+++ w2/q19/Program.fs
@@ -2,7 +2,11 @@
 
 open System
 
+let length lst =
+    (0, lst) ||> List.fold (fun st _ -> st + 1)
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    length [1;2;3;4] |> printfn "%A"
+    Console.ReadKey() |> ignore
     0 // return an integer exit code
