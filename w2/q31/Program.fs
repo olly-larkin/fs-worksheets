6ce4384e42e90e316872b1a46d5829458f3f6722
Olly Larkin <ollylemonlarkin@gmail.com>
2020-01-16 13:59:03 +0000

    q31

---
+++ w2/q31/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let sequences a b =
+    List.allPairs [a..b] [a..b]
+    |> List.collect (fun (e1, e2) -> if (e1 < e2) then [[e1..e2]] else [])
+
+[<EntryPoint>]
+let main argv =
+    sequences 2 6 |> printfn "%A"
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
