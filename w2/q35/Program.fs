b5b1b0de0affe1974554e6dd00b9f3915841e972
Olly Larkin <ollylemonlarkin@gmail.com>
2020-01-16 21:05:20 +0000

    made tick

--- w2/q35/Program.fs
+++ w2/q35/Program.fs
@@ -7,19 +7,17 @@ let printTable tab =
 
 let tabulate tab =
     let newList n v = [1 .. n] |> List.map (fun _ -> v)
-    
-    let colWidth = 
-        ([], tab) ||> List.fold (fun acc arr ->
+    let colWidth =
+        let colWidthFold acc arr = 
             let newAcc = List.append acc <| newList (List.length arr - List.length acc) 0
             let newArr = List.append arr <| newList (List.length acc - List.length arr) ""
             ([], List.zip newArr newAcc) ||> List.fold (fun acc (str, size) -> acc @ [List.max [size; String.length str]])
-        )
-
-    ([], tab) ||> List.fold (fun acc arr ->
-        acc @ [([], arr @ List.replicate (List.length colWidth - List.length arr) "" |> List.zip colWidth) ||> List.fold (fun acc (width, str) ->
+        ([], tab) ||> List.fold colWidthFold
+    let tabulateFold acc arr =
+        let extendStringsFold acc (width, str) =
             acc @ [String.concat "" [str; String.replicate (width - String.length str) " "]]
-        )]
-    )
+        acc @ [([], arr @ List.replicate (List.length colWidth - List.length arr) "" |> List.zip colWidth) ||> List.fold extendStringsFold]
+    ([], tab) ||> List.fold tabulateFold
 
 [<EntryPoint>]
 let main argv =
