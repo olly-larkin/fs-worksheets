a6498a0220d57d96e7543c17af90aa19a7575be8
Olly Larkin <ollylemonlarkin@gmail.com>
2020-01-19 13:24:46 +0000

    ws3q1

---
+++ w3/q1/Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+type Person = {
+    Age: int;
+    FirstName: string;
+    FamilyName: string;
+    Email: string
+}
+
+let newEmail em person = {person with Email = em}
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
