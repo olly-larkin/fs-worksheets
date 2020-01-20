df31642fbd7331b8d00ab42fe5ebac7e68c5040a
Olly Larkin <ollylemonlarkin@gmail.com>
2020-01-20 11:25:26 +0000

    loads better tick 2

--- w2/tick/oll16.fs
+++ w2/tick/oll16.fs
@@ -2,44 +2,27 @@
 
 open System
 
-type State<'a> =
-    | Running of ('a list * 'a list)
-    | Finished of ('a list * 'a list)
-
-module State =
-    let toOption x =
-        match x with
-        | Finished ([], _) -> None
-        | Finished arrs -> Some arrs
-        | Running ([], _) -> None
-        | Running arrs -> Some arrs
-
 let lexNGram (ngram: (char list * bool) list) (cLst: char list) : (char list * char list) option=
     let takeIfInChars chars (acc,lst) : (char list * char list) option =
         match lst with
-        | [] -> None
-        | hd::tl ->
-            match List.tryFind ((=) hd) chars with
-            | None -> None
-            | _ -> Some (acc @ [hd], tl)
-    let rec takeWhileInChars chars (acc,lst) =
+        | hd::tl when List.contains hd chars -> Some (acc @ [hd], tl)
+        | _ -> None
+
+    let rec takeWhileInChars chars (acc,lst) first =
         match lst with
-        | [] -> (acc, lst)
-        | hd::tl ->
-            match List.tryFind ((=) hd) chars with
-            | None -> (acc, lst)
-            | _ -> (acc @ [hd], tl) |> takeWhileInChars chars
+        | hd::tl when List.contains hd chars -> takeWhileInChars chars (acc @ [hd], tl) false
+        | _ when first -> None
+        | _ -> Some (acc, lst)
+
     let tryMatch state (charsLst,canRepeat) =
         match state with
-        | Finished _ -> state
-        | Running lsts ->
-            match takeIfInChars charsLst lsts with
-            | None -> Finished lsts
-            | Some lsts' ->
-                if canRepeat
-                then Running (takeWhileInChars charsLst lsts')
-                else Running lsts' 
-    (Running ([], cLst), ngram) ||> List.fold tryMatch |> State.toOption
+        | None -> None
+        | Some (tokL, matchL) ->
+            if canRepeat
+            then takeWhileInChars charsLst (tokL, matchL) true
+            else takeIfInChars charsLst (tokL, matchL)
+
+    (Some ([], cLst), ngram) ||> List.fold tryMatch
 
 type Lexer = char list -> (char list * char list) option
 
@@ -84,9 +67,11 @@ let main argv =
         [
             Seq.toList "15.7 rabbits"
             Seq.toList "\"rabbits\" 15.7"
+            Seq.toList "\"rabbits 15.7"
             Seq.toList "11"
             Seq.toList "15. 7"
+            Seq.toList "15.7"
         ]
-    ((), testBatch) ||> List.fold (fun _ lst -> (lexNGram integerLit <|> lexNGram stringLit) lst |> printfn "%A") |> ignore
+    ((), testBatch) ||> List.fold (fun _ lst -> lexNGram stringLit lst |> printfn "%A") |> ignore
     Console.ReadKey() |> ignore
     0 // return an integer exit code
