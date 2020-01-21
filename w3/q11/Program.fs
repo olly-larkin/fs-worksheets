78ba843890a987858cd179ef5898522cab38d255
Olly Larkin <ollylemonlarkin@gmail.com>
2020-01-21 13:07:53 +0000

    q11

---
+++ w3/q11/Program.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let primeFactors n =
+    let rec primeRecurse c n = 
+        if c * c > n
+        then [n]
+        else
+            if n % c = 0
+            then c :: (primeRecurse c (n/c))
+            else primeRecurse (c+1) n
+    n |> primeRecurse 2
+
+[<EntryPoint>]
+let main argv =
+    primeFactors 12 |> printfn "%A"
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
