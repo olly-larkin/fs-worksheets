2c5489bfc0a041694d2617ca9f669eb9dbab3261
Olly Larkin <ollylemonlarkin@gmail.com>
2020-01-21 12:51:46 +0000

    q10

---
+++ w3/q10/Program.fs
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let histogram (data: string) =
+    data
+    |> Seq.toList
+    |> List.groupBy id
+    |> Map.ofList
+    |> Map.map (fun _ -> List.length) 
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
